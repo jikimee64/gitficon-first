d4b7fcdc3007ba3c2fdda3110bdd2d7ac4b324ba
jikimee64 <jikimee64@gmail.com>
2024-05-31 23:45:04 +0900

    chore: ecr-code-deploy.yml 추가

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 echo "--------------- 서버 배포 시작 -----------------"
-cd /home/ubuntu/order-system
-sudo fuser -k -n tcp 8080 || true
-nohup java -jar app.jar > ./output.log 2>&1 &
+docker stop ordersystem-server || true
+docker rm ordersystem-server || true
+docker pull 891376927503.dkr.ecr.ap-northeast-2.amazonaws.com//ordersystem-server:latest
+docker run -d --name ordersystem-server -p 8080:8080 891376927503.dkr.ecr.ap-northeast-2.amazonaws.com//ordersystem-server:latest
 echo "--------------- 서버 배포 끝 -----------------"
