704d949d74b13b1157dde166a0a714ef1ef32392
jikimee64 <jikimee64@gmail.com>
2024-06-11 14:53:56 +0900

    chore: start.sh 모니터링 포트 추가

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -4,5 +4,5 @@ echo "--------------- 서버 배포 시작 -----------------"
 docker stop ordersystem-server || true
 docker rm ordersystem-server || true
 docker pull 891376927503.dkr.ecr.ap-northeast-2.amazonaws.com/ordersystem-server:latest
-docker run -d --name ordersystem-server -p 8080:8080 891376927503.dkr.ecr.ap-northeast-2.amazonaws.com/ordersystem-server:latest
+docker run -d --name ordersystem-server -p 8080:8080 -p 9292:9292 891376927503.dkr.ecr.ap-northeast-2.amazonaws.com/ordersystem-server:latest
 echo "--------------- 서버 배포 끝 -----------------"
